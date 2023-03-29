d0192eeabd86b7bf5a35a48075457b8167f79170
ilbey <ilbey@ILBEYLAPTOP>
2023-03-29 15:39:09 +0300

    dfg

--- PersonelEkleme/Irem.cs
+++ PersonelEkleme/Irem.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-
-public class Class1
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+public class Irem()
 {
 	public Irem()
 	{
-		public void Irem ()
+		public void Irem2()
 		{
-
+			global::System.Windows.Forms.MessageBox.Show("Selam");
 		}
 	}
 }
